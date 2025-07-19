f3a124fca82f0125a0a7ad5cbcb367acd4d55c72
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-07-19 13:23:02 +0300

    server.js updated

--- src/server.js
+++ src/server.js
@@ -1,30 +1,26 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import dotenv from 'dotenv';
-import { initMongoConnection } from './db/initMongoConnection.js';
+
 import contactsRouter from './routers/contactsRouter.js';
 import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 
-dotenv.config();
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use(pino());
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  })
+);
 
-// Routes
 app.use('/api/contacts', contactsRouter);
 
-// Middlewares
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
-
-initMongoConnection().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-});
+export default app;
