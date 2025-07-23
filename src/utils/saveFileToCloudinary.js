dd5a9d5f08f3a1811e214838828431dffd11cabc
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-07-24 00:20:26 +0300

    feat: setup cloudinary and multer for file uploads

---
+++ src/utils/saveFileToCloudinary.js
@@ -0,0 +1,13 @@
+import cloudinary from 'cloudinary';
+import fs from 'node:fs/promises';
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const saveFileToCloudinary = async (file) => {
+  const response = await cloudinary.v2.uploader.upload(file.path);
+  await fs.unlink(file.path);
+  return response.url;
+};
